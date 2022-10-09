73e1ce9bbf04c6f40866b93a59815662694cf625
quayen01 <quayen01@pfw.edu>
2022-10-09 15:15:33 -0400

    Updated Code Hopeffully its simpler

--- Motor_Driver.py
+++ Motor_Driver.py
@@ -1,22 +1,14 @@
-# Based on: https://www.raspberrypi.org/forums/viewtopic.php?t=242928\.
+# Emmanuel Quaye
+# RPI 4B
+# This code drive the NEMA 17 motor stepper motor using the TB6600 Driver
 #
-# Software to drive 4 wire stepper motor using a TB6600 Driver
-# PRi - RPi 3B
-#
-# Route 3.3 VDC to the controller "+" input for each: ENA, PUL, and DIR
-#
-# Connect GPIO pins as shown below) to the "-" input for each: ENA, PUL, and DIR
-#
-#
+# 3.3 -> to the controller "+" input for each: ENA, PUL+, and DIR+
 from time import sleep
 import RPi.GPIO as GPIO
 #
 PUL = 17  # Stepper Drive Pulses
-DIR = 27  # Controller Direction Bit (High for Controller default / LOW to Force a Direction Change).
-ENA = 22  # Controller Enable Bit (High to Enable / LOW to Disable).
-# DIRI = 14  # Status Indicator LED - Direction
-# ENAI = 15  # Status indicator LED - Controller Enable
-#
+DIR = 27  # Controller Direction port (High for Controller default / LOW to Force a Direction Change).
+Enable = 22  # The enable port
 # NOTE: Leave DIR and ENA disconnected, and the controller WILL drive the motor in Default direction if PUL is applied.
 # 
 GPIO.setmode(GPIO.BCM)
@@ -24,84 +16,71 @@ GPIO.setmode(GPIO.BCM)
 #
 GPIO.setup(PUL, GPIO.OUT)
 GPIO.setup(DIR, GPIO.OUT)
-GPIO.setup(ENA, GPIO.OUT)
-# GPIO.setup(DIRI, GPIO.OUT)
-# GPIO.setup(ENAI, GPIO.OUT)
+GPIO.setup(Enable, GPIO.OUT)
 #
 print('PUL = GPIO 17 - RPi 3B-Pin #11')
 print('DIR = GPIO 27 - RPi 3B-Pin #13')
 print('ENA = GPIO 22 - RPi 3B-Pin #15')
-# print('ENAI = GPIO 14 - RPi 3B-Pin #8')
-# print('DIRI = GPIO 15 - RPi 3B-Pin #10')
-
 #
-print('Initialization Completed')
-#
-# Could have usesd only one DURATION constant but chose two. This gives play options.
-durationFwd = 5000 # This is the duration of the motor spinning. used for forward direction
-durationBwd = 5000 # This is the duration of the motor spinning. used for reverse direction
-print('Duration Fwd set to ' + str(durationFwd))
-print('Duration Bwd set to ' + str(durationBwd))
-#
-delay = 0.0000001 # This is actualy a delay between PUL pulses - effectively sets the mtor rotation speed.
-print('Speed set to ' + str(delay))
-#
-cycles = 1000 # This is the number of cycles to be run once program is started.
-cyclecount = 0 # This is the iteration of cycles to be run once program is started.
-print('number of Cycles to Run set to ' + str(cycles))
+print('Ready')
 #
 #
-def forward():
-    GPIO.output(ENA, GPIO.HIGH)
-    GPIO.output(ENAI, GPIO.HIGH)
-    print('ENA set to HIGH - Controller Enabled')
+def forward(speed, distance):
+    rotation = 0
+    speed = (speed * 0.0000001) # This is actualy a delay between PUL pulses - effectively sets the mtor rotation speed.
+    print('Speed set to ' + str(speed))
+    GPIO.output(Enable, GPIO.HIGH)
+    print('Enable set to HIGH - Controller Enabled')
     #
     sleep(.5) # pause due to a possible change direction
     GPIO.output(DIR, GPIO.LOW)
-    GPIO.output(DIRI, GPIO.LOW)
-    print('DIR set to LOW - Moving Forward at ' + str(delay))
+    print('Moving Foward at ' + str(speed) + 'F/s')
+    print('Distance Fwd set to ' + str(distance) + 'F')
     print('Controller PUL being driven.')
-    for x in range(durationFwd): 
+    #
+    for x in range(distance): 
         GPIO.output(PUL, GPIO.HIGH)
-        sleep(delay)
+        sleep(speed)
         GPIO.output(PUL, GPIO.LOW)
-        sleep(delay)
-    GPIO.output(ENA, GPIO.LOW)
-    GPIO.output(ENAI, GPIO.LOW)
-    print('ENA set to LOW - Controller Disabled')
+        sleep(speed)
+        rotation = (rotation + 1)
+    GPIO.output(Enable, GPIO.LOW)
+    #
+    print('Enable set to LOW - Controller Disabled')
     sleep(.5) # pause for possible change direction
-    return
+    print('running clock-wise')
+    
+    return rotation
 #
 #
-def reverse():
-    GPIO.output(ENA, GPIO.HIGH)
-    GPIO.output(ENAI, GPIO.HIGH)
-    print('ENA set to HIGH - Controller Enabled')
+def reverse(speed, distance):
+    rotation = 0
+    speed = (speed * 0.0000001) # This is actualy a delay between PUL pulses - effectively sets the mtor rotation speed.
+    print('Speed set to ' + str(speed))
+    GPIO.output(Enable, GPIO.HIGH)
+    print('Enable set to HIGH - Controller Enabled')
     #
     sleep(.5) # pause due to a possible change direction
     GPIO.output(DIR, GPIO.HIGH)
-    GPIO.output(DIRI, GPIO.HIGH)
-    print('DIR set to HIGH - Moving Backward at ' + str(delay))
+    print('Moving Backward at ' + str(speed) + 'F/s')
+    print('Distance Bwd set to ' + str(distance) + 'F')
     print('Controller PUL being driven.')
     #
-    for y in range(durationBwd):
+    for y in range(distance):
         GPIO.output(PUL, GPIO.HIGH)
-        sleep(delay)
+        sleep(speed)
         GPIO.output(PUL, GPIO.LOW)
-        sleep(delay)
-    GPIO.output(ENA, GPIO.LOW)
-    GPIO.output(ENAI, GPIO.LOW)
-    print('ENA set to LOW - Controller Disabled')
+        sleep(speed)
+        rotation = (rotation + 1)
+    GPIO.output(Enable, GPIO.LOW)
+    #
+    print('Enable set to LOW - Controller Disabled')
     sleep(.5) # pause for possible change direction
-    return
+    print('running in counter clock-wise')
+    return rotation
 
-while cyclecount < cycles:
-    forward()
-    reverse()
-    cyclecount = (cyclecount + 1)
-    print('Number of cycles completed: ' + str(cyclecount))
-    print('Number of cycles remaining: ' + str(cycles - cyclecount))
-#
+def stopMotor():
+    GPIO.output(Enable, GPIO.LOW)
+    print('Enable set to LOW - Controller Enabled')
+
 GPIO.cleanup()
-print('Cycling Completed')
-#
