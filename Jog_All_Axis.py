52b9ad7193a83973a8166a271249237e6a2712c4
quayen01 <113059255+quayen01@users.noreply.github.com>
2022-11-18 14:58:16 -1000

    Updated_Jog_Axis

--- Jog_All_Axis.py
+++ Jog_All_Axis.py
@@ -7,7 +7,7 @@
 # import your modules here
 from time import sleep
 import NEMA_Driver as Nema
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
 # use the set_Up_Board method to set up the rasbery pi
 Nema.set_Up_Board()
@@ -17,35 +17,41 @@ Nema.set_Up_Board()
 # 21: X-Motor_DIR, 23: Z-Motor_PUL, 24: Z-Motor_DIR, 22: Enable,
 # 26: X-limit, 19: Y-limit,
 # Leave DIR+ and ENA+ disconnected, and the controller WILL drive the motor in Default direction if PUL is applied.
+
+# The test bench location informations 1st and 2nd row (2nd row is @ X = 300) i.e 1st row in back, 2nd in front
+Water_Meter_Location_1 = [1500, 1500, 1500]
+Water_Meter_Location_2 = [1500, 1500, 1500]
+
 meters_Per_Row = 3
 
 Nema.Camera_X = c_X = 0
 Nema.Camera_Y = c_Y = 0
 Nema.Camera_Z = c_Z = 0
 
-
 Motor_Pin = [17, 27, 5, 6, 20, 21, 23, 24, 22] # all the motor and enable pins you want initialized
-limit_Switches = [26, 19, 1] # all the stop switch pins
-
-# define seven pins for controlin the jogging, last three for distance amount in increasing order
-control_Pins = [2,4,6,8,10,12,14,16,18] # these are arbitrary for now.
+#limit_Switches = [4, 26, 19] # all the stop switch pins
+limit_Switches = [4, 26, 19] # all the stop switch pins pin 4 needs a resistor
 
 # use the set_GPIO_Out method to set up GIO outputs
 Nema.set_GPIO_Out(Motor_Pin)
 
 # use the set_GPIO_In method to set up GIO inputs
 Nema.set_GPIO_In(limit_Switches)
-Nema.set_GPIO_In(control_Pins)
+
+#Set the 8th number in the motor_Pin array as the enable pin correspinding to the real world pin
+Nema.Enable = Motor_Pin[8]
 
 rotation = 0 # define rotation
 dTT = 0 # define distance to travel
+
 Homed = False
+
+def toggle_distance(control_Pin):
+    #use keyboard button to send input
+    if(1==1)
 
 while True:
     # This file will move the x axis a set amount..just for jogging pourposes
-
-    #limit_Switches[0] = GPIO.input(limit_Switches[0])
-    #limit_Switches[1] = GPIO.input(limit_Switches[1])
 
     if(control_Pins[6] == 1):
         distance = Nema.small_Amount
@@ -56,8 +62,7 @@ while True:
 
     if(Homed != True):
         # Home the test bench
-        Nema.home_Machine(100,Motor_Pin[1],Motor_Pin[0],Motor_Pin[3],Motor_Pin[2],Motor_Pin[5],Motor_Pin[4],Motor_Pin[7],Motor_Pin[6],limit_Switches)
-        Nema.enable_Motor() # re-enable the motors
+        Nema.home_Machine(600,Motor_Pin[1],Motor_Pin[0],Motor_Pin[3],Motor_Pin[2],Motor_Pin[5],Motor_Pin[4],Motor_Pin[7],Motor_Pin[6],limit_Switches)
         # Set the camera location as homed
         c_X = 0
         c_Y = 0
